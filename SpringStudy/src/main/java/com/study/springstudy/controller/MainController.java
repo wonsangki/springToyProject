9da03bce7eb85d271cb4925b1538a5b4f555630f
wonsangKi <dev.wonsang@gmail.com>
2022-11-11 16:10:53 +0900

    test commit

--- SpringStudy/src/main/java/com/study/springstudy/controller/MainController.java
+++ SpringStudy/src/main/java/com/study/springstudy/controller/MainController.java
@@ -1,7 +1,16 @@
 package com.study.springstudy.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
+    @GetMapping("/")
+    public ResponseEntity<String> main(){
+        return new ResponseEntity<>("OK THANK YOU", HttpStatus.OK);
+    }
 }
